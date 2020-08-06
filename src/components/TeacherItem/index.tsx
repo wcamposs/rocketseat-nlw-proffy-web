958252fdcbe113f565960c7ea9486fef938ef9b8
Willian Campos <williancampos1995@gmail.com>
2020-08-06 00:46:19 -0300

    adding funcionalities and server integration.

--- src/components/TeacherItem/index.tsx
+++ src/components/TeacherItem/index.tsx
@@ -3,35 +3,56 @@ import React from 'react';
 import whatsappIcon from '../../assets/images/icons/whatsapp.svg';
 
 import './styles.css';
+import api from '../../services/api';
 
-function TeacherItem () {
+export interface Teacher {
+    id: number;
+    avatar: string;
+    bio: string;
+    cost: number;
+    name: string;
+    subject: string;
+    whatsapp: string;
+}
+
+export interface TeacherItemProps {
+    teacher: Teacher;
+}
+
+const TeacherItem: React.FunctionComponent<TeacherItemProps> = ({ teacher }) => {
+    function createNewConnection() {
+        api.post('connections', {
+            user_id: teacher.id,
+        });
+    }
+
     return (
         <article className="teacher-item">
-                    <header>
-                        <img src="https://pbs.twimg.com/profile_images/1281394127180111872/3_Js4Xf3_400x400.jpg" alt="Willian Campos"/>
-                        <div>
-                            <strong>Willian Campos</strong>
-                            <span>Química</span>
-                        </div>
-                    </header>
+            <header>
+                <img src={teacher.avatar} alt={teacher.name} />
+                <div>
+                    <strong>{teacher.name}</strong>
+                    <span>{teacher.subject}</span>
+                </div>
+            </header>
 
-                    <p>
-                        Entusiasta das melhores tecnologias de química avançada.
-                        <br /><br />
-                        Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências. Mais de 200.000 pessoas já passaram por uma das minhas explosões.
-                    </p>
+            <p>{teacher.bio}</p>
 
-                    <footer>
-                        <p>
-                            Preço/hora
-                            <strong>R$ 100,00</strong>
-                        </p>
-                        <button type="button">
-                            <img src={whatsappIcon} alt="Whatsapp" />
+            <footer>
+                <p>
+                    Preço/hora
+                            <strong>R$ {teacher.cost}</strong>
+                </p>
+                <a
+                    target="_blank"
+                    onClick={createNewConnection}
+                    href={`https://wa.me/${teacher.whatsapp}`}
+                >
+                    <img src={whatsappIcon} alt="Whatsapp" />
                             Entrar em contato
-                        </button>
-                    </footer>
-                </article>
+                </a>
+            </footer>
+        </article>
     )
 }
 
